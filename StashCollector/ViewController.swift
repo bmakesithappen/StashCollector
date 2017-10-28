80d1805239a541eef6ea785ce6e9ef36b405eac5
Bernard Desert <bdesert@etsy.com>
2017-10-28 15:03:55 -0400

    First Push creating repo and project

---
+++ StashCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StashCollector
+//
+//  Created by Bernard Désert on 10/28/17.
+//  Copyright © 2017 Bernard Désert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
