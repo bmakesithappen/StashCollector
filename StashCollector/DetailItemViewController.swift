7679b10644b3aac85d27761f689cb768c85d4b5b
Bernard Desert <bdesert@etsy.com>
2017-11-17 14:18:27 -0500

    Added TODO based on new save segue for DetailItemViewController

--- StashCollector/DetailItemViewController.swift
+++ StashCollector/DetailItemViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class DetailItemViewController: UIViewController {
     
-    // TODO: Have value save from DataPicker and display in this viewController.  
+    // TODO: Have value save from DataPicker and display in this viewController.
+    // TODO: When "save" his tapped we want to show the StashListViewController and that specific item added "When I save it I want to see it in the big list"
     
     
     @IBOutlet weak var conditionPicker: UIPickerView!
